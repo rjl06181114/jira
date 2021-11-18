337bda310ff56a9af2b3841ce28b5dd9eed2c28f
renjialei <782527057@qq.com>
2021-11-18 16:51:47 +0800

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
   return (
     <div className="App">
@@ -16,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {" "}
+          Learn React{" "}
         </a>
       </header>
     </div>
